864309f8cffef2f01a202c3503cdfe29f23d4b9a
sophiashuv <45092554+sophiashuv@users.noreply.github.com>
2019-05-25 22:13:35 +0300

    Update btree.py

--- TicTacToe/btree.py
+++ TicTacToe/btree.py
@@ -1,27 +1,8 @@
 class Tree:
+    """
+    Class for tree representation
+    """
     def __init__(self, root):
         self.key = root
         self.left_child = None
         self.right_child = None
-
-    def insert_left(self, new_node):
-        if self.left_child is None:
-            self.left_child = new_node
-        else:
-            t = new_node
-            t.left_child = self.left_child
-            self.left_child = t
-
-    def insert_right(self, new_node):
-        if self.right_child is None:
-            self.right_child = new_node
-        else:
-            t = new_node
-            t.right_child = self.right_child
-            self.right_child = t
-
-    def get_right_child(self):
-        return self.right_child
-
-    def get_left_child(self):
-        return self.left_child
