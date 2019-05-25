fcf327fc96279287f1c775dd715520cecc5ecd3d
sophiashuv <45092554+sophiashuv@users.noreply.github.com>
2019-05-25 19:27:05 +0300

    Update btnode.py

--- TicTacToe/btnode.py
+++ TicTacToe/btnode.py
@@ -1,1 +1,7 @@
-pass
+class Node:
+    """docstring for BTNode."""
+    def __init__(self, item):
+        self.left = None
+        self.right = None
+        self.item = item
+        self.sum = 0
